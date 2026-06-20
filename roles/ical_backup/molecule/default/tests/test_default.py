bb79f76fccfb0fda74b6a8dd300ae3e71dbee63c
Bodo Schulz <bodo@boone-schulz.de>
2026-06-20 17:28:11 +0200

    add ical_backup role (#5)
    * add ical_backup role

---
+++ roles/ical_backup/molecule/default/tests/test_default.py
@@ -0,0 +1,34 @@
+
+from __future__ import annotations, unicode_literals
+
+import os
+
+import testinfra.utils.ansible_runner
+from helper.molecule import get_vars, infra_hosts, local_facts
+
+testinfra_hosts = infra_hosts(host_name="instance")
+
+# --- tests -----------------------------------------------------------------
+
+
+def test_directories(host, get_vars):
+
+    dirs = ["/usr/local/opt/nextcloud-ical-backup", "/etc/nextcloud-ical-backup"]
+
+    for _dir in dirs:
+        f = host.file(_dir)
+        assert f.is_directory
+
+
+def test_files(host, get_vars):
+
+    files = [
+        "/etc/nextcloud-ical-backup/config.toml",
+        "/usr/bin/nextcloud-ical-backup",
+        "/usr/lib/systemd/system/nextcloud-ical-backup.timer",
+        "/usr/lib/systemd/system/nextcloud-ical-backup.service",
+    ]
+
+    for _file in files:
+        f = host.file(_file)
+        assert f.is_file
